2b06825203c5089aed48fa51719161bb94dd9d00
Miria Rafante <m063244@yahoo.com.br>
2021-05-26 22:08:33 -0300

    Update example.py

--- example.py
+++ example.py
@@ -3,36 +3,40 @@ from my_pseknc import Pseknc
 from kmers import Kmers
 import os
 
-input_file_path = "input_file.fasta"
+input_fasta = "input_file.fasta"
 folder_for_output = os.path.join(os.getcwd(), "output")
 
 """
 Converting fasta file into physicochemical properties 
 """
 encoder = FastaToPc3mer(folder_for_output=folder_for_output)
-encoder.convert_to_pc3mer(input_file_path)
+encoder.convert_to_pc3mer(input_fasta)
 
 """
 Converting fasta file into physicochemical statistics 
 """
 encoder = FastaToPc3mer(folder_for_output=folder_for_output)
-encoder.convert_to_pc3mer_stats(input_file_path)
+encoder.convert_to_pc3mer_stats(input_fasta)
 
 
 """
 Converting fasta file into PseKNC - I 
 """
-outputFile = folder_for_output + "/pseknc.csv"
+outputFile = os.path.join(folder_for_output, "pseknc.csv")
 
 encoder = Pseknc()
-encoder.encode_fasta_into_pseknc(input_file_path, outputFile)
+encoder.encode_fasta_into_pseknc(input_fasta, outputFile)
 
 
 """
 Converting fasta file into Several k-mers 
 """
-k_values = list(range(1,7))
-outputFile = folder_for_output + "/1_to_6_mers.csv"
+# defining list of ks
+k_start = 1
+k_end = 3
+k_values = list(range(k_start, k_end+1))
+
+outputFile = os.path.join(folder_for_output, f"{k_start}_to_{k_end}_mers.csv")
 
 encoder = Kmers()
-encoded = encoder.convertFastaIntoSeveralKmers(input_file_path,k_values,outputFile)
+encoded = encoder.convertFastaIntoSeveralKmers(input_fasta, k_values, outputFile)
